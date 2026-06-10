414f7f7eeee4f87ce79388d3d8cdd862c57e3e3a
Arsenty Halimovsky <ghost12@yandex.ru>
2026-06-10 08:40:11 +0300

    Create a new project

---
+++ AsyncAwaitPractice/AsyncAwaitPractice/AsyncAwaitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitPracticeApp.swift
+//  AsyncAwaitPractice
+//
+//  Created by Арсентий Халимовский on 10.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitPractice/AsyncAwaitPractice/ContentView.swift
@@ -0,0 +1,17 @@
+/* Project to understand async await better
+ 
+ 
+ 
+ 
+ 
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	
+    var body: some View {
+        
+    }
+}
